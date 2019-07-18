7a64c50dd2397a16045dbc202ec4c0feed31df12
sancho20021 <sancho20021@gmail.com>
2019-07-18 17:37:34 +0300

    bugs fixed

--- app1/models.py
+++ app1/models.py
@@ -3,5 +3,6 @@ from django.db import models
 
 # Create your models here.
 class App1(models.Model):
+    name = models.CharField(max_length=255)
     text = models.TextField()
     ended = models.BooleanField(default=False)
--- app1/views.py
+++ app1/views.py
@@ -24,21 +24,31 @@ def write(request):
             poem = App1()
             poem.text = ""
             poem.save()
+            return render(request, 'writeANewPoem.html', {'poem': poem})
         else:
             poem = random.choice(poems)
-        return render(request, 'writeAPoem.html', {'poem': poem})
+            if poem.text=='':
+                return render(request, 'writeAPoem.html', {'poem': poem.text,
+                                                           'poemId': poem.id, 'poemName': poem.name})
+            else:
+                return render(request, 'writeAPoem.html', {'poem': poem.text.splitlines()[-1],
+                                                       'poemId': poem.id, 'poemName':poem.name})
     if request.method == 'POST':
-        poemId=request.POST['poemId']
-        poem = App1.objects.get(pk=poemId)
-        if ('button1' in request.POST) and (request.POST['button1'] == 'Добавить'):
-            poem.text = poem.text + request.POST['text'] + '\n'
-        if ('button2' in request.POST) and (request.POST['button2'] == 'Закончить'):
-            poem.text = poem.text + request.POST['text']
-            poem.ended = True
-        if('button3' in request.POST) and request.POST['button3'] == 'Новая':
+        if 'button1' in request.POST or 'button2' in request.POST:
+            poemId = request.POST['poemId']
+            poem = App1.objects.get(pk=poemId)
+            poemName = request.POST['poemName']
+            if ('button1' in request.POST) and (request.POST['button1'] == 'Добавить'):
+                poem.text = poem.text + request.POST['text'] + '\n'
+                poem.name = poemName
+            if ('button2' in request.POST) and (request.POST['button2'] == 'Закончить'):
+                poem.text = poem.text + request.POST['text']
+                poem.name = poemName
+                poem.ended = True
+        else:
             poem = App1()
             poem.text = ""
             poem.save()
-            return render(request, 'writeAPoem.html', {'poem': poem})
+            return render(request, 'writeANewPoem.html', {'poem': poem})
         poem.save()
         return redirect('/read')
